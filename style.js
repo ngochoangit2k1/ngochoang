18f011a02c626c8f5038666f643b446297cd6e15
ngochoangit2k1 <59520070+ngochoangit2k1@users.noreply.github.com>
2021-07-12 02:25:19 +0700

    upload

--- style.js
+++ style.js
@@ -2,13 +2,20 @@ var headers = document.getElementById('header');
 console.log(headers)
 var mobileBar = document.getElementById('mobile-bar');
 console.log(mobileBar)
+var overView = document.getElementById('overview');
+console.log(overView)
+var flexMenu = document.getElementById('flex-menu');
 
 mobileBar.onclick = function(){
     var isClose = headers.clientHeight === 60;
     if (isClose){
         headers.style.height = '200px';
-
+        flexMenu.style.opacity = '1';
     }else{
         headers.style.height = '60px';
+        flexMenu.style.opacity = '0';
     }
+}
+overView.onclick = function(){
+    headers.style.height = '60px';
 }
